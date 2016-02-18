d66e5001e1fb0c36baa7f71c4151aa1b14bccf1c
Grant Campanelli <grantcampanelli@gmail.com>
2016-02-18 13:12:47 -0800

    Added new data model

---
+++ CashTab-V1/Transaction.swift
@@ -0,0 +1,17 @@
+//
+//  Transaction.swift
+//  CashTab-V1
+//
+//  Created by Grant Campanelli on 2/18/16.
+//  Copyright © 2016 Kevin Tsui. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Transaction)
+public class Transaction: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
